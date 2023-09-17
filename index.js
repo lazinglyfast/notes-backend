fa0f5f9306b896161082b3710576371b9d4b06a3
diogofriggo <diogofriggobr@gmail.com>
2023-09-17 10:24:33 +0200

    frontend-build

--- index.js
+++ index.js
@@ -7,67 +7,82 @@ app.use(cors())
 app.use(json())
 app.use(express.static("build"))
 
-app.get("/", (_request, response) => {
-  response.send("<h1>Hello, World!</h1>")
+app.get("/", (_req, res) => {
+  res.send("<h1>Hello, World!</h1>")
 })
 
-app.get("/api/notes", (_request, response) => {
+app.get("/api/notes", (_req, res) => {
   Note.find({}).then((notes) => {
-    response.json(notes)
+    res.json(notes)
   })
 })
 
-app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
-    response.json(note)
-  } else {
-    response
-      .status(404)
-      .send(`Notes with id ${id} not found`)
-      .end()
-  }
+app.get("/api/notes/:id", (req, res) => {
+  Note.findById(req.params.id).then(note => {
+    // const note = notes.find(note => note.id === id)
+    // if (note) {
+    res.json(note)
+    // } else {
+    //   res
+    //     .status(404)
+    //     .send(`Notes with id ${id} not found`)
+    //     .end()
+    // }
+  })
 })
 
-app.delete("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-  response.status(204).end()
+app.delete("/api/notes/:id", (req, res) => {
+  Note.findById(req.params.id).then(note => {
+    note.deleteOne()
+    res.status(204).end()
+  })
+  // const id = Number(req.params.id)
+  // notes = notes.filter(note => note.id !== id)
+  // res.status(204).end()
 })
 
-const generateId = () => {
-  const maxId = notes.reduce((max, n) => n.id > max ? n.id : max, Number.MIN_VALUE)
-  return maxId + 1
-}
+// const generateId = () => {
+//   const maxId = notes.reduce((max, n) => n.id > max ? n.id : max, Number.MIN_VALUE)
+//   return maxId + 1
+// }
 
-app.post("/api/notes", (request, response) => {
-  let note = request.body
-  if (!note.content) {
-    return response.status(400).json({
+app.post("/api/notes", (req, res) => {
+  const data = req.body
+  if (!data.content) {
+    return res.status(400).json({
       error: "content is missing",
     })
   }
-  note = {
-    content: note.content,
-    important: note.important || false,
-    id: generateId(),
-  }
-  notes.push(note)
-  response.json(note)
+  const note = new Note({
+    content: data.content,
+    important: data.important || false,
+    // id: generateId(),
+  })
+  note.save().then((note) => {
+    res.json(note)
+  })
+  // notes.push(data)
 })
 
 app.put("/api/notes/:id", (req, res) => {
-  const note = req.body
-  if (!notes.find(p => p.id == note.id)) {
-    res.status(400) // person does not exist
-  }
-
-  notes = notes
-    .filter(p => p.id != note.id)
-    .concat(note)
-
-  res.json(note)
+  const data = req.body
+  Note.findById(req.params.id).then(note => {
+    note.content = data.content
+    note.important = data.important
+    note.save().then(note => {
+      res.json(note)
+    })
+  })
+  // const note = req.body
+  // if (!notes.find(p => p.id == note.id)) {
+  //   res.status(400) // person does not exist
+  // }
+  //
+  // notes = notes
+  //   .filter(p => p.id != note.id)
+  //   .concat(note)
+  //
+  // res.json(note)
 })
 
 const PORT = process.env.PORT || 3002
